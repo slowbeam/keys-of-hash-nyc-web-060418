621f8c2b08117229097921134b67f2d7e2a36461
slowbeam <sedwardscode@gmail.com>
2018-05-31 14:12:13 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    animals = []
+    self.each do |animal, place|
+      if arguments.include?(place)
+        animals << animal
+      end
+    end
+    return animals
   end
 end
